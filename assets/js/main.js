3772f92b6441390159a9b4b6f0c3bb8228cb2c7d
chancemusselman <musselman.chance@icloud.com>
2018-09-07 00:10:02 -0700

    Basic Layout, No Responsivness at the moment

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+console.log("test");
+
+
+
+
+
+
+// CSS FONTS FROM GOOGLE FONTS https://fonts.google.com/featured/2016+Fonts+Refresh?query=chance&category=Sans+Serif,Display
+// font-family: 'Rubik', sans-serif;
+// font-family: 'Pacifico', cursive;
+// font-family: 'Quicksand', sans-serif;
+// font-family: 'Inconsolata', monospace;
+// font-family: 'Cabin', sans-serif;
+// font-family: 'VT323', monospace;
+// font-family: 'Nunito', sans-serif;
+// font-family: 'Nunito Sans', sans-serif;
+// font-family: 'Ubuntu', sans-serif;
